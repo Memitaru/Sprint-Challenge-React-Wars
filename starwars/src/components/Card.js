3a67e1401c5b90ca33ac097a17736e4a55e450c4
Memitaru <amandatwah@gmail.com>
2019-03-10 20:08:24 -0700

    Added points of info to card component

--- starwars/src/components/Card.js
+++ starwars/src/components/Card.js
@@ -4,7 +4,21 @@ import './card.css';
 const Card = props => {
     return (
         <div className="card">
-        <p>card</p>
+        <h2>
+            {props.character.name}
+        </h2>
+        <div className="profile">
+        <div className="info">
+            <p>Birth Year: {props.character.birth_year}</p>
+            <p>Hair Color: {props.character.hair_color}</p>
+            <p>Eye Color: {props.character.eye_color}</p>
+        </div>
+        <div className="info">
+        <p>Gender: {props.character.gender}</p>
+        <p>Height: {props.character.height}</p>
+        <p>Mass: {props.character.mass}</p>
+        </div>
+        </div>
         </div>
     )
 }
--- starwars/src/components/List.js
+++ starwars/src/components/List.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Card from './Card';
 
+import "./StarWars.css";
+
 const List = props => {
     return (
         <div className="list">
             {props.characters.map(character => (
-                <Card  key={character.name} characterDetails={character} />
+                <Card  key={character.name} character={character} />
             ))}
         </div>
     )
